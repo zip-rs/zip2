7a559457431f3418bcc0bcb9ba68559b3b57de99
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2024-05-03 14:31:10 -0400

    add benchmarks

--- src/spec.rs
+++ src/spec.rs
@@ -58,7 +58,7 @@ impl CentralDirectoryEnd {
         const BYTES_BETWEEN_MAGIC_AND_COMMENT_SIZE: u64 = HEADER_SIZE - 6;
         let file_length = reader.seek(io::SeekFrom::End(0))?;
 
-        let search_upper_bound = file_length.saturating_sub(MAX_HEADER_AND_COMMENT_SIZE);
+        let search_upper_bound = 0;
 
         if file_length < HEADER_SIZE {
             return Err(ZipError::InvalidArchive("Invalid zip header"));
