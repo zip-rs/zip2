be5836e14dfc5587122dd4cbcc8ff6a0a79e151e
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-05-10 15:01:11 -0700

    test: Fix unused imports by moving them inside the cfg-gated test

--- tests/repro_old423.rs
+++ tests/repro_old423.rs
@@ -1,11 +1,10 @@
-use std::env::temp_dir;
-use std::io;
-use zip::result::ZipResult;
-use zip::ZipArchive;
-
 #[cfg(all(unix, feature = "_deflate-any"))]
 #[test]
-fn repro_old423() -> ZipResult<()> {
+fn repro_old423() -> zip::result::ZipResult<()> {
+    use std::env::temp_dir;
+    use std::io;
+    use zip::ZipArchive;
+
     let mut v = Vec::new();
     v.extend_from_slice(include_bytes!("data/lin-ub_iwd-v11.zip"));
     let mut archive = ZipArchive::new(io::Cursor::new(v)).expect("couldn't open test zip file");
