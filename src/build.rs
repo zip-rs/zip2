e0b6be7eee0170c7983a2225354fc5943aba630a
SapryWenInera <pedro_americo@id.uff.br>
2024-05-11 23:56:18 -0300

    Add initial error message for when io logic is missing do to feature missing

--- src/build.rs
+++ src/build.rs
@@ -4,4 +4,6 @@ fn main() {
     if var("CARGO_FEATURE_DEFLATE_MINIZ").is_ok() {
         println!("cargo:warning=Feature `deflate-miniz` is deprecated; replace it with `deflate`");
     }
+    #[cfg(not(any(feature = "sync")))]
+    compile_error!("Missing Required feature");
 }
