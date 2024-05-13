c056382b5c17b1d1f49081067579f9c998d79602
SapryWenInera <pedro_americo@id.uff.br>
2024-05-12 23:01:14 -0300

    Add compile error for missing/conflicting features

--- src/build.rs
+++ src/build.rs
@@ -4,6 +4,9 @@ fn main() {
     if var("CARGO_FEATURE_DEFLATE_MINIZ").is_ok() {
         println!("cargo:warning=Feature `deflate-miniz` is deprecated; replace it with `deflate`");
     }
-    #[cfg(not(any(feature = "sync")))]
+    #[cfg(not(any(feature = "sync", feature = "tokio")))]
     compile_error!("Missing Required feature");
+
+    #[cfg(all(feature = "sync", feature = "tokio"))]
+    compile_error!("The features sync and tokio cannot be used together")
 }
