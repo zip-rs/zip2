cb50ca121350457c5fba1f0a7c3b3e907c7d2b2d
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-03 21:12:54 -0700

    test: Bug fix? Explicitly type int that may become as large as 3 << 29

--- src/write.rs
+++ src/write.rs
@@ -288,7 +288,7 @@ impl<'a> arbitrary::Arbitrary<'a> for FileOptions<'a, ExtendedFileOptions> {
         #[cfg(feature = "deflate-zopfli")]
         if options.compression_method == CompressionMethod::Deflated && bool::arbitrary(u)? {
             options.zopfli_buffer_size =
-                Some(1 << u.int_in_range(8..=29)? * if bool::arbitrary(u)? { 2 } else { 3 });
+                Some(if bool::arbitrary(u)? { 2 } else { 3 } << u.int_in_range(8..=29)?);
         }
         u.arbitrary_loop(Some(0), Some((u16::MAX / 4) as u32), |u| {
             options
