a53d8e3fbd88a9590195cbc1ddb28f344416e081
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-07 21:51:52 +0000

    Increase iteration to 15M runs or 15 minutes

--- fuzz-until-converged.sh
+++ fuzz-until-converged.sh
@@ -6,8 +6,8 @@ while [[ $iters_without_improvement -lt $MAX_ITERS_WITHOUT_IMPROVEMENT ]]; do
   cp -r "fuzz/corpus/fuzz_$1" "fuzz/corpus/fuzz_$1_old"
   cargo fuzz run --all-features "fuzz_$1" "fuzz/corpus/fuzz_$1" -- \
     -dict=fuzz/fuzz.dict -max_len="$2" -rss_limit_mb=8192 \
-    -fork="$(nproc || getconf NPROCESSORS_ONLN)" -runs=10000000 \
-    -max_total_time=600
+    -fork="$(nproc || getconf NPROCESSORS_ONLN)" -runs=15000000 \
+    -max_total_time=900
   ./recursive-fuzz-cmin.sh "$1" "$2"
   if diff "fuzz/corpus/fuzz_$1" "fuzz/corpus/fuzz_$1_old"; then
     iters_without_improvement=$(( iters_without_improvement + 1 ))
