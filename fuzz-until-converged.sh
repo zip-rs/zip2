3422c9586bbceafa4d52d3dfa6c251ef732f592a
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-07 20:47:04 +0000

    Use longer fuzz iterations to reduce overhead of repeated cmin

--- fuzz-until-converged.sh
+++ fuzz-until-converged.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 rm -r "fuzz/corpus/fuzz_$1_old"
-MAX_ITERS_WITHOUT_IMPROVEMENT=5
+MAX_ITERS_WITHOUT_IMPROVEMENT=3
 iters_without_improvement=0
 while [[ $iters_without_improvement -lt $MAX_ITERS_WITHOUT_IMPROVEMENT ]]; do
   cp -r "fuzz/corpus/fuzz_$1" "fuzz/corpus/fuzz_$1_old"
   cargo fuzz run --all-features "fuzz_$1" "fuzz/corpus/fuzz_$1" -- \
     -dict=fuzz/fuzz.dict -max_len="$2" -rss_limit_mb=8192 \
-    -fork="$(nproc || getconf NPROCESSORS_ONLN)" -max_total_time=300
+    -fork="$(nproc || getconf NPROCESSORS_ONLN)" -runs=10000000 \
+    -max_total_time=600
   ./recursive-fuzz-cmin.sh "$1" "$2"
   if diff "fuzz/corpus/fuzz_$1" "fuzz/corpus/fuzz_$1_old"; then
     iters_without_improvement=$(( iters_without_improvement + 1 ))
