e950efc8c5bcb297c7352be36d80ebb365af2021
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-13 04:44:05 +0000

    Extend timeout during fuzzing

--- recursive-fuzz-cmin.sh
+++ recursive-fuzz-cmin.sh
@@ -7,7 +7,7 @@ while true; do
   cp -r "fuzz/corpus/fuzz_$1_iter_${i}" "fuzz/corpus/fuzz_$1_iter_${i}.bak"
   mkdir "fuzz/corpus/fuzz_$1_iter_${j}"
   cargo fuzz cmin --all-features "fuzz_$1" "fuzz/corpus/fuzz_$1_iter_${i}" -- \
-    -dict=fuzz/fuzz.dict -max_len="$2" -rss_limit_mb=8192 "fuzz/corpus/fuzz_$1_iter_${j}"
+    -dict=fuzz/fuzz.dict -max_len="$2" -rss_limit_mb=8192 -timeout=30 "fuzz/corpus/fuzz_$1_iter_${j}"
   if diff "fuzz/corpus/fuzz_$1_iter_${i}.bak" "fuzz/corpus/fuzz_$1_iter_${j}"; then
     # Last iteration made no difference, so we're done
     rm -r "fuzz/corpus/fuzz_$1"
