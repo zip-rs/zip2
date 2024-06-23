949682ee5f50ebe4e19812a573e0019519f76b59
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-22 19:22:23 -0700

    test(fuzz): Add one restart with no dictionary, in case the dictionary's bias is limiting our coverage

--- build-fuzz-corpus-multiple-restarts.sh
+++ build-fuzz-corpus-multiple-restarts.sh
@@ -2,9 +2,9 @@
 set -euxo pipefail
 ncpus=$(nproc || getconf NPROCESSORS_ONLN)
 ncpus=$(( ncpus / ( 1 + $(cat /sys/devices/system/cpu/smt/active))))
-RESTARTS=10
+NORMAL_RESTARTS=10
 mv "fuzz/corpus/fuzz_$1" "fuzz/corpus/fuzz_$1_pre_fresh_blood" || true
-for i in $(seq 1 $RESTARTS); do
+for i in $(seq 1 $NORMAL_RESTARTS); do
   mv "fuzz/corpus/fuzz_$1_restart_${i}"/* "fuzz/corpus/fuzz_$1_pre_fresh_blood" || true
   echo "RESTART ${i}"
   mkdir "fuzz/corpus/fuzz_$1"
@@ -14,8 +14,13 @@ for i in $(seq 1 $RESTARTS); do
   mkdir "fuzz/corpus/fuzz_$1_restart_${i}" || true
   mv "fuzz/corpus/fuzz_$1"/* "fuzz/corpus/fuzz_$1_restart_${i}"
 done
-mkdir "fuzz/corpus/fuzz_$1"
-for i in $(seq 1 $RESTARTS); do
+
+mv "fuzz/corpus/fuzz_$1_restart_dictionaryless"/* "fuzz/corpus/fuzz_$1_pre_fresh_blood" || true
+echo "DICTIONARY-LESS RESTART"
+  cargo fuzz run --all-features "fuzz_$1" "fuzz/corpus/fuzz_$1" -- \
+    -max_len="$2" -fork="$ncpus" -max_total_time=5100 -runs=100000000
+
+for i in $(seq 1 $NORMAL_RESTARTS); do
   mv "fuzz/corpus/fuzz_$1_restart_${i}"/* "fuzz/corpus/fuzz_$1"
   rmdir "fuzz/corpus/fuzz_$1_restart_${i}"
 done
