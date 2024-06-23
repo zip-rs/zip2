cfbcb6090cb9fc1c35b35fee165173e25b074b34
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-22 17:50:23 -0700

    test(fuzz): Use outputs from previous size as recombination material for new size

--- build-fuzz-corpus-multiple-sizes.sh
+++ build-fuzz-corpus-multiple-sizes.sh
@@ -2,7 +2,7 @@
 mkdir "fuzz/corpus/fuzz_$1_recombination_sources" || true
 mv "fuzz/corpus/fuzz_$1/*" "fuzz/corpus/fuzz_$1_recombination_sources"
 for size in "${@:2}"; do
-  rm -rf "fuzz/corpus/fuzz_$1_pre_fresh_blood"
+  mv "fuzz/corpus/fuzz_$1_pre_fresh_blood/*" "fuzz/corpus/fuzz_$1_recombination_sources" || true
   ./build-fuzz-corpus-multiple-restarts.sh "$1" "$size"
   find "fuzz/corpus/fuzz_$1_pre_fresh_blood" -type -f -size "-${size}c" -exec mv '{}' "fuzz/corpus/fuzz_$1" ';'
   find "fuzz/corpus/fuzz_$1_recombination_sources" -type -f -size "-${size}c" -exec mv '{}' "fuzz/corpus/fuzz_$1" ';'
