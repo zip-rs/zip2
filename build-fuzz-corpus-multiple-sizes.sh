085b9ca6914a5689d4ff4e7dac028b998125324e
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-22 20:04:41 -0700

    test(fuzz): Bug fix in find command

--- build-fuzz-corpus-multiple-sizes.sh
+++ build-fuzz-corpus-multiple-sizes.sh
@@ -16,7 +16,7 @@ for size in "${@:2}"; do
   rm -rf "fuzz/corpus/fuzz_$1_pre_fresh_blood" || true
   mv "fuzz/corpus/fuzz_$1/*" "fuzz/corpus/fuzz_$1_recombination_sources" || true
   ./build-fuzz-corpus-multiple-restarts.sh "$1" "$size"
-  find "fuzz/corpus/fuzz_$1_recombination_sources" -type -f -size "-${size}c" -exec mv '{}' "fuzz/corpus/fuzz_$1" ';'
+  find "fuzz/corpus/fuzz_$1_recombination_sources" -type f -size "-${size}c" -exec mv '{}' "fuzz/corpus/fuzz_$1" ';'
   ./fuzz-until-converged.sh "$1" "$size"
 done
 echo "$(date): FINISHED"
