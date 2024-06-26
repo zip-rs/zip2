cdc5f71f66d60d07028d48061ecb88e05f712822
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-25 20:25:21 -0700

    test(fuzz): Fix an off-by-one error affecting corpus entries of the exact maximum length

--- build-fuzz-corpus-multiple-sizes.sh
+++ build-fuzz-corpus-multiple-sizes.sh
@@ -10,7 +10,7 @@ for size in "${@:2}"; do
   rm -rf "fuzz/corpus/fuzz_$1_pre_fresh_blood" || true
   find "fuzz/corpus/fuzz_$1" -type f -exec mv '{}' "fuzz/corpus/fuzz_$1_recombination_sources" ';' || true
   ./build-fuzz-corpus-multiple-restarts.sh "$1" "$size"
-  find "fuzz/corpus/fuzz_$1_recombination_sources" -type f -size "-${size}c" -exec mv '{}' "fuzz/corpus/fuzz_$1" ';'
+  find "fuzz/corpus/fuzz_$1_recombination_sources" -type f -size "-$((size + 1))c" -exec mv '{}' "fuzz/corpus/fuzz_$1" ';'
   ./fuzz-until-converged.sh "$1" "$size"
 done
 echo "$(date): FINISHED"
