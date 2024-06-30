11db7c83ce75c30c77f58cbf747d74e59459c54a
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2024-06-29 19:10:21 -0700

    test(fuzz): Refactor: avoid moving final restart out of fuzz_write and back

--- build-fuzz-corpus-multiple-restarts.sh
+++ build-fuzz-corpus-multiple-restarts.sh
@@ -41,14 +41,13 @@ echo "$(date): RESTART WITH DICTIONARY AND 0-2 BYTE CORPUS"
 tar -xvzf "fuzz/012byte.tar.gz" -C "fuzz/corpus/fuzz_$1"
 cargo fuzz run --all-features "fuzz_$1" "fuzz/corpus/fuzz_$1" -- \
   -dict=fuzz/fuzz.dict -max_len="$2" -fork="$ncpus" -max_total_time=5100 -runs=100000000
-find "fuzz/corpus/fuzz_$1" -type f -exec mv '{}' "fuzz/corpus/fuzz_$1_restart_012byte" ';'
 
 echo "$(date): MERGING CORPORA"
 for i in $(seq 1 $NORMAL_RESTARTS); do
   find "fuzz/corpus/fuzz_$1_restart_${i}" -type f -exec mv '{}' "fuzz/corpus/fuzz_$1" ';'
   rm -rf "fuzz/corpus/fuzz_$1_restart_${i}"
 done
-SPECIAL_RESTARTS=("012byte" "dictionaryless_012byte" "dictionaryless")
+SPECIAL_RESTARTS=("dictionaryless_012byte" "dictionaryless")
 for i in "${SPECIAL_RESTARTS[@]}"; do
   find "fuzz/corpus/fuzz_$1_restart_${i}" -type f -exec mv '{}' "fuzz/corpus/fuzz_$1" ';'
   rm -rf "fuzz/corpus/fuzz_$1_restart_${i}"
