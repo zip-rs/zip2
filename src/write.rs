e4f9b183bb2921fdea96afa1cfb38f877c1cc991
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2024-07-16 17:37:14 -0400

    remove a few more dyns

--- src/write.rs
+++ src/write.rs
@@ -86,7 +86,7 @@ impl<W: Write> Write for MaybeEncrypted<W> {
     }
 }
 
-enum GenericZipWriter<W: Write + Seek> {
+enum GenericZipWriter<W: Write> {
     Closed,
     Storer(MaybeEncrypted<W>),
     #[cfg(feature = "deflate-flate2")]
