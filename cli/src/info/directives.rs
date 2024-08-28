c5f5ebbcd3c13a9128209bcda76f03863e4c26c1
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2024-08-28 17:11:01 -0400

    refactor modules of compiled formatting

--- cli/src/info/directives.rs
+++ cli/src/info/directives.rs
@@ -4,7 +4,10 @@ use std::{
 };
 
 use super::formats::FormatValue;
-use crate::{CommandError, WrapCommandErr};
+use crate::{
+    args::info::{ParseableDirective, ParseableFormatComponent, ParseableFormatSpec},
+    CommandError, WrapCommandErr,
+};
 
 pub trait Writeable {
     fn write_to(&self, out: &mut dyn Write) -> Result<(), io::Error>;
@@ -72,13 +75,116 @@ where
     }
 }
 
+pub mod compiled {
+    use super::*;
+
+    enum CompiledFormatComponent<F> {
+        Directive(F),
+        ContiguousLiteral(String),
+    }
+
+    impl<F> CompiledFormatComponent<F>
+    where
+        F: DirectiveFormatter,
+    {
+        pub fn write_component<'a>(
+            &self,
+            data: <F as DirectiveFormatter>::Data<'a>,
+            mut out: impl Write,
+        ) -> Result<(), CommandError> {
+            match self {
+                Self::Directive(d) => d.write_directive(data, &mut out),
+                Self::ContiguousLiteral(lit) => out
+                    .write_all(lit.as_bytes())
+                    .wrap_err_with(|| format!("failed to write literal {lit:?} to output")),
+            }
+        }
+    }
+
+    pub trait CompiledFormat {
+        type Spec: ParseableDirective;
+        type Fmt: DirectiveFormatter;
+
+        fn from_directive_spec(spec: Self::Spec) -> Result<Self::Fmt, CommandError>;
+    }
+
+    pub struct CompiledFormatSpec<F> {
+        pub components: Vec<CompiledFormatComponent<F>>,
+    }
+
+    impl<F> CompiledFormatSpec<F> {
+        pub fn is_empty(&self) -> bool {
+            self.components.is_empty()
+        }
+    }
+
+    impl<F> CompiledFormatSpec<F>
+    where
+        F: DirectiveFormatter,
+    {
+        pub fn from_spec<CF>(
+            spec: ParseableFormatSpec<<CF as CompiledFormat>::Spec>,
+        ) -> Result<Self, CommandError>
+        where
+            CF: CompiledFormat<Fmt = F>,
+        {
+            let ParseableFormatSpec {
+                components: spec_components,
+            } = spec;
+
+            let mut components: Vec<CompiledFormatComponent<F>> = Vec::new();
+            for c in spec_components.into_iter() {
+                match c {
+                    ParseableFormatComponent::Directive(d) => {
+                        let d = CF::from_directive_spec(d)?;
+                        components.push(CompiledFormatComponent::Directive(d));
+                    }
+                    ParseableFormatComponent::Escaped(s) => match components.last_mut() {
+                        Some(CompiledFormatComponent::ContiguousLiteral(ref mut last_lit)) => {
+                            last_lit.push_str(s);
+                        }
+                        _ => {
+                            components
+                                .push(CompiledFormatComponent::ContiguousLiteral(s.to_string()));
+                        }
+                    },
+                    ParseableFormatComponent::Literal(new_lit) => match components.last_mut() {
+                        Some(CompiledFormatComponent::ContiguousLiteral(ref mut last_lit)) => {
+                            last_lit.push_str(new_lit.as_str());
+                        }
+                        _ => {
+                            components.push(CompiledFormatComponent::ContiguousLiteral(new_lit));
+                        }
+                    },
+                }
+            }
+
+            Ok(Self { components })
+        }
+
+        pub fn execute_format<'a>(
+            &self,
+            data: <F as DirectiveFormatter>::Data<'a>,
+            mut out: impl Write,
+        ) -> Result<(), CommandError>
+        where
+            <F as DirectiveFormatter>::Data<'a>: Clone,
+        {
+            for c in self.components.iter() {
+                c.write_component(data.clone(), &mut out)?
+            }
+            Ok(())
+        }
+    }
+}
+
 pub mod entry {
     use super::{
         super::formats::{
             ByteSizeValue, CompressionMethodValue, FileTypeValue, FormatValue, NameString,
             UnixModeValue,
         },
-        FormatDirective,
+        DirectiveFormatter, FormatDirective,
     };
     use crate::extract::receiver::EntryData;
 
@@ -159,6 +265,74 @@ pub mod entry {
         }
         fn value_formatter(&self) -> ByteSizeValue {
             self.0
+        }
+    }
+
+    pub mod compiled {
+        use super::{super::compiled::CompiledFormat, *};
+        use crate::{args::info::EntryFormatDirective, CommandError};
+
+        use std::io::Write;
+
+        /// Used for type erasure by removing the lifetime-bounded associated type.
+        trait EntryDirectiveFormatter {
+            fn write_entry_directive<'a>(
+                &self,
+                data: &EntryData<'a>,
+                out: &mut dyn Write,
+            ) -> Result<(), CommandError>;
+        }
+
+        impl<CF> EntryDirectiveFormatter for CF
+        where
+            CF: for<'a> DirectiveFormatter<Data<'a> = &'a EntryData<'a>>,
+        {
+            fn write_entry_directive<'a>(
+                &self,
+                data: &EntryData<'a>,
+                out: &mut dyn Write,
+            ) -> Result<(), CommandError> {
+                self.write_directive(data, out)
+            }
+        }
+
+        /// This re-implements the generic trait using the type-erased boxed vtable.
+        pub struct CompiledEntryDirective(Box<dyn EntryDirectiveFormatter>);
+
+        impl DirectiveFormatter for CompiledEntryDirective {
+            type Data<'a> = EntryData<'a>;
+
+            fn write_directive<'a>(
+                &self,
+                data: Self::Data<'a>,
+                out: &mut dyn Write,
+            ) -> Result<(), CommandError> {
+                self.0.write_entry_directive(&data, out)
+            }
+        }
+
+        pub struct CompiledEntryFormat;
+
+        impl CompiledFormat for CompiledEntryFormat {
+            type Spec = EntryFormatDirective;
+            type Fmt = CompiledEntryDirective;
+
+            fn from_directive_spec(
+                spec: EntryFormatDirective,
+            ) -> Result<CompiledEntryDirective, CommandError> {
+                Ok(CompiledEntryDirective(match spec {
+                    EntryFormatDirective::Name => Box::new(EntryNameField(NameString)),
+                    EntryFormatDirective::FileType(f) => Box::new(FileTypeField(FileTypeValue(f))),
+                    EntryFormatDirective::UncompressedSize(f) => {
+                        Box::new(UncompressedSizeField(ByteSizeValue(f)))
+                    }
+                    EntryFormatDirective::UnixMode(f) => Box::new(UnixModeField(UnixModeValue(f))),
+                    EntryFormatDirective::CompressionMethod(f) => {
+                        Box::new(CompressionMethodField(CompressionMethodValue(f)))
+                    }
+                    _ => todo!(),
+                }))
+            }
         }
     }
 }
