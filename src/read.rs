9422277eb90e1b2bf0c8c533d1cc16cf4d86cfd3
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2025-03-15 14:42:51 -0700

    Fix: need to convert to OsStr first, then to Path
    Signed-off-by: Chris Hennick <4961925+Pr0methean@users.noreply.github.com>

--- src/read.rs
+++ src/read.rs
@@ -480,7 +480,7 @@ pub(crate) fn make_symlink(outpath: &PathBuf, target: Vec<u8>) -> ZipResult<()> 
         } else {
             false
         };
-        let target = Path::new(&target);
+        let target = Path::new(OsStr::new(&target));
         if target_is_dir {
             std::os::windows::fs::symlink_dir(target, outpath.as_path())?;
         } else {
