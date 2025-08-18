32da0d38f8c234d5deb78afc729a256818cf3a78
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2025-08-18 11:36:47 -0700

    chore: Move deprecated annotations to fix a Clippy warning
    Signed-off-by: Chris Hennick <4961925+Pr0methean@users.noreply.github.com>

--- src/types.rs
+++ src/types.rs
@@ -437,14 +437,13 @@ impl DateTime {
 }
 
 #[cfg(feature = "time")]
+#[deprecated(
+    since = "2.5.0",
+    note = "use `TryFrom<PrimitiveDateTime> for DateTime` instead"
+)]
 impl TryFrom<OffsetDateTime> for DateTime {
     type Error = DateTimeRangeError;
 
-    #[allow(useless_deprecated)]
-    #[deprecated(
-        since = "2.5.0",
-        note = "use `TryFrom<PrimitiveDateTime> for DateTime` instead"
-    )]
     fn try_from(dt: OffsetDateTime) -> Result<Self, Self::Error> {
         Self::try_from(PrimitiveDateTime::new(dt.date(), dt.time()))
     }
@@ -467,14 +466,13 @@ impl TryFrom<PrimitiveDateTime> for DateTime {
 }
 
 #[cfg(feature = "time")]
+#[deprecated(
+    since = "2.5.0",
+    note = "use `TryFrom<DateTime> for PrimitiveDateTime` instead"
+)]
 impl TryFrom<DateTime> for OffsetDateTime {
     type Error = ComponentRange;
 
-    #[allow(useless_deprecated)]
-    #[deprecated(
-        since = "2.5.0",
-        note = "use `TryFrom<DateTime> for PrimitiveDateTime` instead"
-    )]
     fn try_from(dt: DateTime) -> Result<Self, Self::Error> {
         PrimitiveDateTime::try_from(dt).map(PrimitiveDateTime::assume_utc)
     }
