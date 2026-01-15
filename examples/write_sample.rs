ebc9b6ed31d88ac5f881ffdcccbe444c4f734afe
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-01-15 14:58:30 -0800

    Remove excess whitespace
    Signed-off-by: Chris Hennick <4961925+Pr0methean@users.noreply.github.com>

--- examples/write_sample.rs
+++ examples/write_sample.rs
@@ -91,25 +91,3 @@ Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
