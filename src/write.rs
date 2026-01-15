6399cca0556b43c2c7b7f10828b8b23c6c2e5aef
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-01-15 15:08:09 -0800

    Finish rename of clamp_opt
    Signed-off-by: Chris Hennick <4961925+Pr0methean@users.noreply.github.com>

--- src/write.rs
+++ src/write.rs
@@ -1743,7 +1743,7 @@ impl<W: Write + Seek> GenericZipWriter<W> {
                     #[cfg(all(feature = "deflate-zopfli", not(feature = "deflate-flate2")))]
                     let default = 24;
 
-                    let level = clamp_opt(
+                    let level = validate_value_in_range(
                         compression_level.unwrap_or(default),
                         deflate_compression_level_range(),
                     )
@@ -1810,7 +1810,7 @@ impl<W: Write + Seek> GenericZipWriter<W> {
                 }
                 #[cfg(feature = "bzip2")]
                 CompressionMethod::Bzip2 => {
-                    let level = clamp_opt(
+                    let level = validate_value_in_range(
                         compression_level.unwrap_or(bzip2::Compression::default().level() as i64),
                         bzip2_compression_level_range(),
                     )
@@ -1825,7 +1825,7 @@ impl<W: Write + Seek> GenericZipWriter<W> {
                 )),
                 #[cfg(feature = "zstd")]
                 CompressionMethod::Zstd => {
-                    let level = clamp_opt(
+                    let level = validate_value_in_range(
                         compression_level.unwrap_or(zstd::DEFAULT_COMPRESSION_LEVEL as i64),
                         zstd::compression_level_range(),
                     )
@@ -1854,7 +1854,7 @@ impl<W: Write + Seek> GenericZipWriter<W> {
                 }
                 #[cfg(feature = "xz")]
                 CompressionMethod::Xz => {
-                    let level = clamp_opt(compression_level.unwrap_or(6), 0..=9)
+                    let level = validate_value_in_range(compression_level.unwrap_or(6), 0..=9)
                         .ok_or(UnsupportedArchive("Unsupported compression level"))?
                         as u32;
                     Ok(Box::new(move |bare| {
@@ -1871,7 +1871,7 @@ impl<W: Write + Seek> GenericZipWriter<W> {
                 CompressionMethod::Ppmd => {
                     const ORDERS: [u32; 10] = [0, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-                    let level = clamp_opt(compression_level.unwrap_or(7), 1..=9)
+                    let level = validate_value_in_range(compression_level.unwrap_or(7), 1..=9)
                         .ok_or(UnsupportedArchive("Unsupported compression level"))?
                         as u32;
 
