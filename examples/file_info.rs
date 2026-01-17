5756e04264c3f302f2ac571e76297d30822f14bd
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-01-16 16:46:41 -0800

    cargo fmt --all

--- examples/file_info.rs
+++ examples/file_info.rs
@@ -1,4 +1,4 @@
-use anyhow::{Result, anyhow, Context};
+use anyhow::{anyhow, Context, Result};
 use std::fs;
 use std::io::BufReader;
 
@@ -9,7 +9,8 @@ fn main() -> Result<()> {
     }
     let fname_arg = &args[1];
     // Determine a trusted base directory (current working directory).
-    let base_dir = std::env::current_dir().with_context(|| "Could not determine current directory")?;
+    let base_dir =
+        std::env::current_dir().with_context(|| "Could not determine current directory")?;
     // Construct the path relative to the trusted base directory and canonicalize it.
     let candidate_path = base_dir.join(fname_arg);
     // FIXME: still vulnerable to a Time-of-check to time-of-use (TOCTOU) race condition.
@@ -18,10 +19,13 @@ fn main() -> Result<()> {
     // between the canonicalize() and open() calls. This could bypass the starts_with check and lead
     // to path traversal. A fully secure solution is difficult without openat-like functionality
     // (which isn't in std).
-    let path = candidate_path.canonicalize().with_context(|| format!("Could not open {fname_arg:?}"))?;
+    let path = candidate_path
+        .canonicalize()
+        .with_context(|| format!("Could not open {fname_arg:?}"))?;
     if !path.starts_with(&base_dir.canonicalize().unwrap_or(base_dir.clone())) {
         return Err(anyhow!(
-            "Error: refusing to open path outside of base directory: {fname_arg:?}"));
+            "Error: refusing to open path outside of base directory: {fname_arg:?}"
+        ));
     }
     let mut archive = zip::ZipArchive::new(BufReader::new(fs::File::open(&path)?))
         .with_context(|| format!("Could not open {fname_arg:?}"))?;
