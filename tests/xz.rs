ff99ad9f14127504a575316c99cf5ec132ec558b
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-01-23 15:40:57 -0800

    style: cargo fmt

--- tests/xz.rs
+++ tests/xz.rs
@@ -8,11 +8,14 @@ pub fn decompress_xz() {
     let v = include_bytes!("data/xz.zip");
     let mut archive = ZipArchive::new(io::Cursor::new(v)).expect("couldn't open test zip file");
 
-    let mut file = archive.by_name("hello.txt").expect("couldn't find hello.txt in archive");
+    let mut file = archive
+        .by_name("hello.txt")
+        .expect("couldn't find hello.txt in archive");
     assert_eq!("hello.txt", file.name());
 
     let mut content = Vec::new();
-    file.read_to_end(&mut content).expect("failed to read content from hello.txt");
+    file.read_to_end(&mut content)
+        .expect("failed to read content from hello.txt");
     assert_eq!(
         "Hello world\n",
         String::from_utf8(content).expect("content from hello.txt should be valid UTF-8")
