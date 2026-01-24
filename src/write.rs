9e2e04fa1fe3654e2c4f8a72e7b6a1c982ce057e
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-01-24 11:14:49 -0800

    style: cargo fmt

--- src/write.rs
+++ src/write.rs
@@ -2412,7 +2412,7 @@ mod test {
             // won't work with zopfli and no flate2, because DEFLATE is write-only in that cfg
             #[cfg(all(feature = "deflate_zopfli", not(feature = "deflate_flate2")))]
             compression_method: zip::CompressionMethod::Stored,
-    
+
             ..Default::default()
         };
         writer.start_file("foo.txt", options)?;
