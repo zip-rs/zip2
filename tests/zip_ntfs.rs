cc18494cb16a06bb9afa50a7988af19508db5410
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-13 08:51:55 -0800

    Add back deleted assertions
    Signed-off-by: Chris Hennick <4961925+Pr0methean@users.noreply.github.com>

--- tests/zip_ntfs.rs
+++ tests/zip_ntfs.rs
@@ -21,4 +21,19 @@ fn test_ntfs_extra_field_timestamp_parsing() {
             }
         })
         .expect("Expected NTFS extra field in test.txt");
+
+    assert_eq!(timestamp.mtime(), 133_813_273_144_169_390);
+    #[cfg(feature = "nt-time")]
+    assert_eq!(
+        time::UtcDateTime::try_from(timestamp.modified_file_time()).unwrap(),
+        time::macros::datetime!(2025-01-14 11:21:54.416_939_000 UTC)
+    );
+
+    assert_eq!(timestamp.atime(), 0);
+    #[cfg(feature = "nt-time")]
+    assert_eq!(timestamp.accessed_file_time(), nt_time::FileTime::NT_TIME_EPOCH);
+
+    assert_eq!(timestamp.ctime(), 0);
+    #[cfg(feature = "nt-time")]
+    assert_eq!(timestamp.created_file_time(), nt_time::FileTime::NT_TIME_EPOCH);
 }
