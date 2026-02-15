c02fdf1a5c4a6834ab4777429933371d7a186a59
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-15 01:34:28 -0800

    Fix: convert a literal from OffsetDateTime to UtcDateTime in tests/zip_ntfs.rs

--- tests/zip_ntfs.rs
+++ tests/zip_ntfs.rs
@@ -28,7 +28,7 @@ fn test_ntfs_extra_field_timestamp_parsing() {
     #[cfg(feature = "nt-time")]
     {
         const EXPECTED_DATETIME: time::UtcDateTime =
-            time::macros::datetime!(2025-01-14 11:21:54.416_939_000 UTC);
+            time::macros::datetime!(2025-01-14 11:21:54.416_939_000 UTC).to_utc();
         assert_eq!(
             time::UtcDateTime::try_from(timestamp.modified_file_time()).unwrap(),
             EXPECTED_DATETIME
