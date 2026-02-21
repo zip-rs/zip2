31c13f639acea64384b5c478b1cd2f7ea8c88914
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-02-21 13:27:39 -0800

    test: Add a test that validates CRC_TABLE

--- src/zipcrypto.rs
+++ src/zipcrypto.rs
@@ -308,3 +308,14 @@ static CRC_TABLE: [u32; 256] = [
     0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693, 0x54de5729, 0x23d967bf,
     0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94, 0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d,
 ];
+
+#[test]
+fn test_crc_table() {
+    for byte in 0..=(u8::MAX as u32) {
+        let mut crc = byte;
+        for _ in 0..8 {
+            crc = (crc >> 1) ^ ((crc & 1) * 0xEDB88320);
+        }
+        assert_eq!(CRC_TABLE[byte as usize], crc);
+    }
+}
