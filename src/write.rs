929dc88d26053109599efe95fa4a000eb76f35b1
n4n5 <its.just.n4n5@gmail.com>
2026-04-25 16:48:52 -0600

    fix name

--- src/write.rs
+++ src/write.rs
@@ -899,7 +899,7 @@ impl<A: Read + Write + Seek> ZipWriter<A> {
             .try_inner_mut()?
             .seek(SeekFrom::Start(write_position))?;
         let mut new_data = src_data.clone();
-        new_data.file_name_raw = dest_name.as_bytes().into();
+        let dest_name_raw = dest_name.as_bytes();
         new_data.file_name = dest_name.into();
         new_data.header_start = write_position;
         let extra_data_start = write_position
