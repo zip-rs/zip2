6ceaec5fb5e31db492e5fb484c35e7d4b48ad2b4
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-05-03 17:05:44 -0700

    Fix warning: explicit lifetimes

--- src/read/stream.rs
+++ src/read/stream.rs
@@ -148,7 +148,7 @@ impl ZipStreamFileMetadata {
     ///
     /// You can use the [`ZipFile::enclosed_name`] method to validate the name
     /// as a safe path.
-    pub fn name<'a>(&'a self) -> ZipResult<Cow<'a, str>> {
+    pub fn name(&self) -> ZipResult<Cow<str>> {
         self.0.name(&self.1)
     }
 
