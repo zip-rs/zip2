f9c5b49a6be9df55aab8f3ef952fb3164f2945d9
n4n5 <its.just.n4n5@gmail.com>
2026-05-06 09:31:59 +0200

    add new test for normal func

--- src/read/stream.rs
+++ src/read/stream.rs
@@ -232,6 +232,14 @@ impl ZipStreamFileMetadata {
 /// * `data_start`: set to 0
 /// * `external_attributes`: `unix_mode()`: will return None
 pub fn read_zipfile_from_stream<R: Read>(reader: &mut R) -> ZipResult<Option<ZipFile<'_, R>>> {
+    read_zipfile_from_stream_with_options(reader, ZipReadOptions::default())
+}
+
+/// Same as `read_zipfile_from_stream` but with `ZipReadOptions`
+pub fn read_zipfile_from_stream_with_options<'a, R: Read>(
+    reader: &'a mut R,
+    mut options: ZipReadOptions<'a>,
+) -> ZipResult<Option<ZipFile<'a, R>>> {
     // We can't use the typical [`ZipLocalEntryBlock::parse`] method, as we follow separate code paths depending on the
     // "magic" value (since the magic value will be from the central directory header if we've
     // finished iterating over all the actual files).
@@ -251,15 +259,30 @@ pub fn read_zipfile_from_stream<R: Read>(reader: &mut R) -> ZipResult<Option<Zip
 
     let block = block.from_le();
 
-    let (mut result, mut file_name_raw) = ZipFileData::from_local_block(block, reader)?;
+    let (mut data, mut file_name_raw) = ZipFileData::from_local_block(block, reader)?;
 
-    match parse_extra_field(&mut result, &mut file_name_raw) {
+    match parse_extra_field(&mut data, &mut file_name_raw) {
         Ok(..) | Err(ZipError::Io(..)) => {}
         Err(e) => return Err(e),
     }
 
-    let limit_reader = reader.take(result.compressed_size);
-    let crypto_reader = make_crypto_reader(&result, limit_reader, None)?;
+    if options.ignore_encryption_flag {
+        // Always use no password when we're ignoring the encryption flag.
+        options.password = None;
+    } else {
+        // Require and use the password only if the file is encrypted.
+        match (options.password, data.is_encrypted()) {
+            (None, true) => {
+                return Err(ZipError::UnsupportedArchive(ZipError::PASSWORD_REQUIRED));
+            }
+            // Password supplied, but none needed! Discard.
+            (Some(_), false) => options.password = None,
+            _ => {}
+        }
+    }
+
+    let limit_reader = reader.take(data.compressed_size);
+    let crypto_reader = make_crypto_reader(&data, limit_reader, options.password)?;
     let ZipFileData {
         crc32,
         uncompressed_size,
@@ -267,14 +290,12 @@ pub fn read_zipfile_from_stream<R: Read>(reader: &mut R) -> ZipResult<Option<Zip
         #[cfg(feature = "legacy-zip")]
         flags,
         ..
-    } = result;
+    } = data;
 
-    // we cannot decrypt with this function right now
-    // so technically not really useful
-    let vendor_version = result.aes_mode.map(|aes| aes.1);
+    let vendor_version = data.aes_mode.map(|aes| aes.1);
     Ok(Some(ZipFile {
         file_name_raw: Cow::Owned(file_name_raw),
-        data: Cow::Owned(result),
+        data: Cow::Owned(data),
         reader: make_reader(
             compression_method,
             uncompressed_size,
@@ -637,11 +658,40 @@ mod tests {
         let mut reader = Cursor::new(bytes);
         const PASSWORD: &[u8] = b"helloworld";
         let options = ZipReadOptions::new().password(Some(PASSWORD));
+
+        // we simulate the fact that we need the compressed size like a streamed zip
         let result = read_zipfile_from_stream_with_compressed_size_and_options(
             &mut reader,
             compressed_size,
             options,
         );
+        let optional_file = result.unwrap();
+        let mut file = optional_file.unwrap();
+
+        let file_name = file.name().unwrap();
+        assert_eq!(file_name, "secret_data_128");
+
+        const SECRET_CONTENT: &str = "Lorem ipsum dolor sit amet";
+        let mut decrypted_content = String::new();
+        file.read_to_string(&mut decrypted_content)
+            .expect("couldn't read encrypted file");
+        assert_eq!(SECRET_CONTENT, decrypted_content);
+    }
+
+    #[test]
+    #[cfg(feature = "aes-crypto")]
+    fn zip_read_streaming_compressed_and_aes_without_size() {
+        use super::read_zipfile_from_stream_with_options;
+        use crate::ZipReadOptions;
+
+        let bytes = include_bytes!("../../tests/data/aes_archive.zip");
+
+        let mut reader = Cursor::new(bytes);
+        const PASSWORD: &[u8] = b"helloworld";
+        let options = ZipReadOptions::new().password(Some(PASSWORD));
+
+        // the zip already has the compressed size (it's not a streamed zip)
+        let result = read_zipfile_from_stream_with_options(&mut reader, options);
         let optional_file = result.unwrap();
         let mut file = optional_file.unwrap();
 
