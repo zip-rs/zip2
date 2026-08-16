ce9acba1fd5958fc799b4281386fea79879c114f
n4n5 <its.just.n4n5@gmail.com>
2026-08-16 21:24:12 +0200

    fix: wrap in module

--- src/read/readers.rs
+++ src/read/readers.rs
@@ -272,19 +272,29 @@ pub(crate) fn make_reader<R: Read + ?Sized>(
     ))))
 }
 
-#[test]
-fn test_size_reader_enum() {
-    use std::io::Cursor;
-    use std::mem::size_of;
+#[cfg(test)]
+mod tests {
 
-    type R = Cursor<Vec<u8>>;
-    let enum_size = size_of::<ZipFileReader<'_, R>>();
-    eprintln!("{enum_size}");
-    eprintln!("{}", size_of::<io::Take<R>>());
-    eprintln!("{}", size_of::<Crc32Reader<CryptoReader<'_, R>>>());
-    eprintln!(
-        "{}",
-        size_of::<Crc32Reader<Decompressor<io::BufReader<CryptoReader<'_, R>>>>>()
-    );
-    assert!(enum_size <= 32);
+    #[test]
+    fn test_size_reader_enum() {
+        use super::ZipFileReader;
+        use super::Crc32Reader;
+        use std::io::BufReader;
+        use super::CryptoReader;
+        use std::io::Cursor;
+        use std::io::Take;
+        use super::Decompressor;
+        use std::mem::size_of;
+
+        type R = Cursor<Vec<u8>>;
+        let enum_size = size_of::<ZipFileReader<'_, R>>();
+        eprintln!("{enum_size}");
+        eprintln!("{}", size_of::<Take<R>>());
+        eprintln!("{}", size_of::<Crc32Reader<CryptoReader<'_, R>>>());
+        eprintln!(
+            "{}",
+            size_of::<Crc32Reader<Decompressor<BufReader<CryptoReader<'_, R>>>>>()
+        );
+        assert!(enum_size <= 32);
+    }
 }
