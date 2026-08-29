9f9ac3647beadbd04e3d7c0383a1e0a1c546c71c
n4n5 <git@n4n5.dev>
2026-08-29 15:27:43 +0200

    Add system option to SimpleFileOptions
    Signed-off-by: n4n5 <git@n4n5.dev>

--- src/read/zipfile.rs
+++ src/read/zipfile.rs
@@ -375,6 +375,7 @@ impl<'a, R: Read + ?Sized> ZipFile<'a, R> {
         let mut options = SimpleFileOptions::default()
             .large_file(self.compressed_size().max(self.size()) >= ZIP64_BYTES_THR)
             .compression_method(self.compression())
+            .system(self.system())
             .external_attributes(self.external_attributes())
             .last_modified_time(
                 self.last_modified()
