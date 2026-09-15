8c31ef3777033b84cad9ea8c98ff8558a97c3d72
n4n5 <its.just.n4n5@gmail.com>
2026-09-15 15:19:40 +0200

    fix: add testing

--- src/format/data_descriptor.rs
+++ src/format/data_descriptor.rs
@@ -92,7 +92,7 @@ impl Zip64DataDescriptorBlock {
             buff[8], buff[9], buff[10], buff[11], buff[12], buff[13], buff[14], buff[15],
         ]);
         let uncompressed_size = u64::from_le_bytes([
-            buff[16], buff[17], buff[17], buff[19], buff[20], buff[21], buff[22], buff[23],
+            buff[16], buff[17], buff[18], buff[19], buff[20], buff[21], buff[22], buff[23],
         ]);
 
         Ok(Self {
@@ -109,5 +109,61 @@ impl Zip64DataDescriptorBlock {
         w.write_all(&self.compressed_size.to_le_bytes())?;
         w.write_all(&self.uncompressed_size.to_le_bytes())?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::format::data_descriptor::Zip64DataDescriptorBlock;
+    use crate::format::data_descriptor::ZipDataDescriptorBlock;
+
+    #[test]
+    fn test_data_descriptor_wrong_sig() {
+        assert_eq!(
+            ZipDataDescriptorBlock::MAGIC.to_le_bytes(),
+            [0x50, 0x4b, 0x07, 0x08]
+        );
+        let data_desc =
+            ZipDataDescriptorBlock::parse(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+        assert!(data_desc.is_err(),);
+
+        // zip64
+        assert_eq!(
+            Zip64DataDescriptorBlock::MAGIC.to_le_bytes(),
+            [0x50, 0x4b, 0x07, 0x08]
+        );
+        let data_desc = Zip64DataDescriptorBlock::parse(&[
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+        ]);
+        assert!(data_desc.is_err(),);
+    }
+
+    #[test]
+    fn test_data_descriptor_parsing() {
+        let raw = [
+            0x50, 0x4b, 0x07, 0x08, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        ];
+        let data_desc = ZipDataDescriptorBlock::parse(&raw).unwrap();
+        assert_eq!(
+            data_desc,
+            ZipDataDescriptorBlock {
+                crc32: u32::from_le_bytes(raw[4..8].try_into().unwrap()),
+                compressed_size: u32::from_le_bytes(raw[8..12].try_into().unwrap()),
+                uncompressed_size: u32::from_le_bytes(raw[12..16].try_into().unwrap()),
+            }
+        );
+        let raw = [
+            0x50, 0x4b, 0x07, 0x08, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23,
+        ];
+        let data_desc = Zip64DataDescriptorBlock::parse(&raw).unwrap();
+        assert_eq!(
+            data_desc,
+            Zip64DataDescriptorBlock {
+                crc32: u32::from_le_bytes(raw[4..8].try_into().unwrap()),
+                compressed_size: u64::from_le_bytes(raw[8..16].try_into().unwrap()),
+                uncompressed_size: u64::from_le_bytes(raw[16..24].try_into().unwrap()),
+            }
+        );
     }
 }
